18a94e038c573dd03f43043150d5f91e63ddfc50
Kili <Kilimanjaro.a2@gmail.com>
2020-02-17 21:08:34 +0900

    Created argument options

--- mask_processor.py
+++ mask_processor.py
@@ -6,15 +6,13 @@ from PIL import Image
 def process_image(
     src_path="data/src.jpg",
     mask_path="data/mask.png",
-    output_path="data/result.png"
-):
+    output_path="data/result.png"):
     try:
         src = np.array(Image.open(src_path).convert("RGBA"))
         mask = np.array(
             Image.open(mask_path)
             .resize(src.shape[1::-1], Image.BILINEAR)
-            .convert("RGBA")
-        )
+            .convert("RGBA"))
 
         src[mask[..., -1] == 0] = [255, 255, 255, 0]
         Image.fromarray(src).save('data/result.png')
@@ -24,9 +22,29 @@ def process_image(
         raise
 
 def main():
-    main_arg_parser = argparse.ArgumentParser(description="The script to crop the area two images intersect")
-    main_arg_parser.parse_args()
-    process_image()
+    main_arg_parser = argparse.ArgumentParser(
+        description="The script to crop the area two images intersect")
+    main_arg_parser.add_argument(
+        '-s',
+        '--source',
+        type=str,
+        default='data/src.jpg',
+        help='The path of a source image')
+    main_arg_parser.add_argument(
+        '-m',
+        '--mask',
+        type=str,
+        default='data/mask.png',
+        help='The path of a mask image')
+    main_arg_parser.add_argument(
+        '-o',
+        '--output',
+        type=str,
+        default='data/result.png',
+        help="The path of a processed image")
+    args = main_arg_parser.parse_args()
+
+    process_image(args.source, args.mask, args.output)
 
 if __name__ == "__main__":
     main()
