dd2f1b4b2b09fdb1dee639f6a35630cc24927caf
Kili <Kilimanjaro.a2@gmail.com>
2020-02-17 20:20:50 +0900

    Added error handling

--- mask_processor.py
+++ mask_processor.py
@@ -1,12 +1,18 @@
+import sys
 import numpy as np
 from PIL import Image
 
-src = np.array(Image.open('data/src.jpg').convert("RGBA"))
-mask = np.array(
-    Image.open('data/mask.png')
-    .resize(src.shape[1::-1], Image.BILINEAR)
-    .convert("RGBA")
-)
-src[mask[..., -1] == 0] = [255, 255, 255, 0]
+try:
+    src = np.array(Image.open('data/src.jpg').convert("RGBA"))
+    mask = np.array(
+        Image.open('data/mask.png')
+        .resize(src.shape[1::-1], Image.BILINEAR)
+        .convert("RGBA")
+    )
 
-Image.fromarray(src).save('data/result.png')
+    src[mask[..., -1] == 0] = [255, 255, 255, 0]
+    Image.fromarray(src).save('data/result.png')
+    print("Created File: data/result.png")
+except:
+    print("Unexpected error:", sys.exc_info()[0])
+    raise
