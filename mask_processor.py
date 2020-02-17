86cf095053553c898dab7ca61995cd1971349ed4
Kili <Kilimanjaro.a2@gmail.com>
2020-02-17 20:29:41 +0900

    Separated image processing codes from a main function

--- mask_processor.py
+++ mask_processor.py
@@ -2,17 +2,28 @@ import sys
 import numpy as np
 from PIL import Image
 
-try:
-    src = np.array(Image.open('data/src.jpg').convert("RGBA"))
-    mask = np.array(
-        Image.open('data/mask.png')
-        .resize(src.shape[1::-1], Image.BILINEAR)
-        .convert("RGBA")
-    )
+def process_image(
+    src_path="data/src.jpg",
+    mask_path="data/mask.png",
+    output_path="data/result.png"
+):
+    try:
+        src = np.array(Image.open(src_path).convert("RGBA"))
+        mask = np.array(
+            Image.open(mask_path)
+            .resize(src.shape[1::-1], Image.BILINEAR)
+            .convert("RGBA")
+        )
 
-    src[mask[..., -1] == 0] = [255, 255, 255, 0]
-    Image.fromarray(src).save('data/result.png')
-    print("Created File: data/result.png")
-except:
-    print("Unexpected error:", sys.exc_info()[0])
-    raise
+        src[mask[..., -1] == 0] = [255, 255, 255, 0]
+        Image.fromarray(src).save('data/result.png')
+        print("Created File: " + output_path)
+    except:
+        print("Unexpected error:", sys.exc_info()[0])
+        raise
+
+def main():
+    process_image()
+
+if __name__ == "__main__":
+    main()
